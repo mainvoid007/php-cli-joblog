d587abdd519c8a68da2ed1e9e350491956a8d641
Olaf Maywald <olaf.maywald@gmail.com>
2017-11-18 08:36:12 +0100

    debug demojob

--- demoJob.php
+++ demoJob.php
@@ -1,17 +1,17 @@
 <?php
-date_default_timezone_set("Europe/Berlin");
+#date_default_timezone_set("Europe/Berlin");
 
 require_once 'src/cronLogClass.php';
 
-$cron = new cronLogClass(__FILE__, $debug = FALSE);
+$cron = new cronLogClass(__FILE__, $debug = TRUE);
 
 $cron->log( "shit happens", 'error');
 $cron->log($cron->get_config()->serverName);
 $cron->log("omg");
-$cron->log($cron->get_config()->pathToLockFolder, 'success');
+$cron->log($cron->get_config()->pathToLockFolder, 'ok');
 
 //$cron->log($cron->get_config()->pathToNoWhere, 1);
-echo $cron->get_config();
+//echo $cron->get_config();
 
 
 ?>
